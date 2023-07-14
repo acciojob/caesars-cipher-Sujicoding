5428b9bc12dad9ec9864bdf0e9dac710d3c82efa
Sujicoding <117893004+Sujicoding@users.noreply.github.com>
2023-07-14 15:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,13 +30,32 @@ const lookup = {
   "?": "?",
   ",": ",",
 };
-
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+ let decodedArr = []; 
+  
+  for (var i = 0; i < encodedStr.length; i++) {
+    var char = encodedStr[i];
+    var charCode = encodedStr.charCodeAt(i);
 
-  return; //return decodedArr
+    // Check if the character is a letter
+    if (/[A-Z]/.test(char)) {
+      // Determine the base code for 'A' or 'a' depending on the case
+      var baseCode = char >= 'A' && char <= 'Z' ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+      
+      // Apply the ROT13 shift
+      var shiftedCode = (charCode - baseCode + 13) % 26;
+      
+      // Convert the shifted code back to a character
+      char = String.fromCharCode(baseCode + shiftedCode);
+    }
+
+    // Append the character to the decoded string
+    decoded += char;
+  }
+
+  return decoded;
 }
+
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
@@ -44,3 +63,6 @@ function rot13(encodedStr) {
 
 // Do not change this line
 window.rot13 = rot13;
+var encodedString = 'EBG13 rknzcyr.';
+var decodedString = rot13(encodedString);
+console.log(decodedString);
